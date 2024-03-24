b0f17e89680c9edc87bba8f0a3b78021a1cff117
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-24 11:16:41 +0200

    install custom font

--- pages/home-screen/home-screen.tsx
+++ pages/home-screen/home-screen.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export function HomeScreen() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>External Home Screen</Text>
+        <Text style={{fontFamily: 'Bangers-Regular', fontSize: 30}}>External Home Screen</Text>
       </View>
     );
   }
