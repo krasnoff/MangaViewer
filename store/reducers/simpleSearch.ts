43bed7fd5f6d66e688cf34ae1cf9e0a2a2e1a751
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-25 20:12:37 +0200

    minor fix - reducer

--- store/reducers/simpleSearch.ts
+++ store/reducers/simpleSearch.ts
@@ -2,13 +2,10 @@
 import { GET_SIMPLE_SEARCH_LOADED } from "../action-type";
 import initialState from "../state";
 
-const SimpleSearchReducer = (state = initialState, action2: any) => {
-    const type: string = action2.type.toString();
-    const res = action2.type.toString() === 'GET_SIMPLE_SEARCH_LOADED';
-
-    if (type == 'GET_SIMPLE_SEARCH_LOADED') {
+const SimpleSearchReducer = (state = initialState, action: any) => {
+    if (action.type == GET_SIMPLE_SEARCH_LOADED) {
         return Object.assign({}, state, {
-            simpleSearchResponse: state.simpleSearchResponse = action2.payload
+            simpleSearchResponse: state.simpleSearchResponse = action.payload
         });
     }
 
