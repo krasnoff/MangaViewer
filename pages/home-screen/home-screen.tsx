d7e3d95a96f7dd2b8cea83aa1235c2f09454fdc1
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-26 21:31:43 +0200

    start parsing data

--- pages/home-screen/home-screen.tsx
+++ pages/home-screen/home-screen.tsx
@@ -1,20 +1,28 @@
 import { Button, ImageBackground, StyleSheet, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { getSimpleSearch } from "../../store/actions/simpleSearch";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BACKGROUND_IMAGE } from "../../assets/images";
+import { SearchResults } from "../../types/search-results";
 
 export function HomeScreen() {
   const dispatch = useDispatch();
   const data = useSelector(state => (state as unknown as any).SimpleSearchResponse);
+  
+  const [status, setStatus] = useState<number>(0);
 
   useEffect(() => {
-    // console.log('useDispatch...')
     dispatch(getSimpleSearch()); 
   }, [dispatch]);
 
   useEffect(() => {
-    console.log('useSelector output result with data...', data)
+    console.log('useSelector output result with data...', data);
+    
+    if (data.simpleSearchResponse.status === 200) {
+      const resData: SearchResults = data.simpleSearchResponse.data;
+    }
+
+    setStatus(data.simpleSearchResponse.status);
   }, [data]);
 
   const buttonPressHandler = () => {
@@ -23,8 +31,9 @@ export function HomeScreen() {
 
   return (
     <ImageBackground source={BACKGROUND_IMAGE} resizeMode="cover" style={styles.image}>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%' }}>
-        <Text style={{fontFamily: 'Bangers-Regular', fontSize: 30}}>External Home Screen</Text>
+      <View style={styles.view}>
+        {status === 503 ? <Text style={styles.text}>Main server is down for maintanence.{'\n'}Please try again later.</Text> : null}
+        
         <Button onPress={() => buttonPressHandler()} title="Press Me"></Button>
       </View>
     </ImageBackground>
@@ -35,5 +44,15 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     justifyContent: 'center',
+  },
+  view: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    width: '100%' 
+  },
+  text: {
+    fontFamily: 'Bangers-Regular', 
+    fontSize: 20
   }
 });
