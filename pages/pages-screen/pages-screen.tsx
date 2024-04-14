ada003ef7215e044823f2973ad6b00883db54f0b
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-04-14 21:27:15 +0300

    start install react native compressor

--- pages/pages-screen/pages-screen.tsx
+++ pages/pages-screen/pages-screen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Dimensions, Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import { Dimensions, ImageBackground, StyleSheet, Text, View } from "react-native";
 import { BACKGROUND_IMAGE } from '../../assets/images';
 import { Daum } from '../../types/search-results';
 import { getChapters } from '../../store/actions/chapters';
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Carousel from 'react-native-reanimated-carousel';
 import { ChapterObj } from '../../types/chapters';
 import CustomSizePage from '../../components/customSizePage';
-import ImageResizer from '@bam.tech/react-native-image-resizer';
+
 
 
 function PagesScreen({ route, navigation }: any): JSX.Element {
@@ -20,14 +20,11 @@ function PagesScreen({ route, navigation }: any): JSX.Element {
     const [baseUrl, setBaseUrl] = useState<string>('');
     const [hash, setHash] = useState<string>('');
     const [dataSaver, setDataSaver] = useState<string[]>([]);
-    const [resizedDataSaver, setResizedDataSaver] = useState<string[]>([]);
-
+    
     const { itemId } = route.params;
 
     const width = Dimensions.get('window').width;
     const height = Dimensions.get('window').height; 
-
-    const resizedDataSaverArr: string[] = [];
 
     useEffect(() => {
       setItem(itemId);
@@ -45,35 +42,8 @@ function PagesScreen({ route, navigation }: any): JSX.Element {
         setBaseUrl(pagesList.baseUrl);
         setHash(pagesList.chapter.hash);
         setDataSaver(pagesList.chapter.dataSaver);
-        // pagesList.chapter.dataSaver.forEach(el => {
-        //   createResizedImage(`${baseUrl}/data-saver/${hash}/${el}`);
-        // });
       }
     }, [data]);
-
-    const createResizedImage = (path: string) => {
-      return ImageResizer.createResizedImage(
-        path,
-        width,
-        height,
-        'JPEG',
-        50,
-        0,
-        undefined
-      )
-        .then((response) => {
-          // response.uri is the URI of the new image that can now be displayed, uploaded...
-          // response.path is the path of the new image
-          // response.name is the name of the new image with the extension
-          // response.size is the size of the new image
-          resizedDataSaverArr.push(response.uri);
-          setResizedDataSaver(resizedDataSaverArr);
-        })
-        .catch((err) => {
-          // Oops, something went wrong. Check that the filename is correct and
-          // inspect err to get more details.
-        });
-    }
 
     return (
       <ImageBackground source={BACKGROUND_IMAGE} resizeMode="cover" style={styles.image}>
@@ -98,9 +68,7 @@ function PagesScreen({ route, navigation }: any): JSX.Element {
                             backgroundColor: '#FFFFFF'
                         }}
                     >
-                        {/* <Image src={`${baseUrl}/data-saver/${hash}/${dataSaver[index]}`}></Image> */}
                         <CustomSizePage uri={`${baseUrl}/data-saver/${hash}/${dataSaver[index]}`}></CustomSizePage>
-                        {/* <CustomSizePage uri={resizedDataSaver[index]}></CustomSizePage> */}
                     </View>
                 )}
             />
