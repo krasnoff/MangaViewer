1e9f797e602e2ff4c55e740887451c11de925e8c
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2024-09-02 21:24:01 +0300

    bug fix - package size

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     project: {
         ios:{},
-        android:{}
+        android:{
+            packageName: 'com.krasnoffkobi.mangaviewer',
+        }
     },
     assets:['./assets/fonts/'],
 }
