e657d7a9b19bb0d70eea3bb9a3c0687da1f5e729
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2024-11-08 11:56:03 +0200

    build login page

--- pages/login-screen/login-screen.tsx
+++ pages/login-screen/login-screen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import ContentPageFrame from '../../components/content-page-frame';
 import styles from '../../styles/content-page-style';
 import PressableLink from '../../components/pressable-link';
@@ -9,9 +9,42 @@ function LoginScreen({ route, navigation }: any): JSX.Element {
         <ContentPageFrame>
             <>
                 <Text style={styles.subTitle}>Login</Text>
+
+                <TextInput
+                    style={stylesForms.input}
+                    placeholder='Email'
+                    inputMode='email'
+                />
+
+                <TextInput
+                    style={stylesForms.input}
+                    placeholder='Password'
+                    secureTextEntry={true}
+                />
+
+                <View style={stylesForms.buttonWrap}><Button
+                    title="Login"
+                /></View>
             </>
         </ContentPageFrame>
     );
 }
+
+const stylesForms = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 0,
+        marginTop: 12,
+        marginBottom: 12,
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10,
+    },
+    buttonWrap: {
+        marginTop: 12,
+        marginBottom: 12,
+        width: 70
+    }
+});
 
 export default LoginScreen;
