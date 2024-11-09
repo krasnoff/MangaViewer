e6ff189b4c4dbb8d6c854caa7706a5073f4ee0f5
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2024-11-09 09:40:02 +0200

    implement useForm

--- pages/login-screen/login-screen.tsx
+++ pages/login-screen/login-screen.tsx
@@ -2,29 +2,78 @@ import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import ContentPageFrame from '../../components/content-page-frame';
 import styles from '../../styles/content-page-style';
-import PressableLink from '../../components/pressable-link';
+import { Controller, useForm } from 'react-hook-form';
+import { LoginFormData } from '../../types/login-form-data';
 
 function LoginScreen({ route, navigation }: any): JSX.Element {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<LoginFormData>({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+    
+    const submitHandler = (data: LoginFormData) => {
+        console.log('submit handler', data)
+    }
+
     return (
         <ContentPageFrame>
             <>
                 <Text style={styles.subTitle}>Login</Text>
 
-                <TextInput
-                    style={stylesForms.input}
-                    placeholder='Email'
-                    inputMode='email'
+                <Controller
+                    control={control}
+                    rules={{
+                        required: 'Email is required',
+                        pattern: {
+                            value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                            message: 'Invalid email address',
+                        },
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                            style={stylesForms.input}
+                            placeholder='Email'
+                            inputMode='email'
+                            onBlur={onBlur}
+                            onChangeText={onChange}
+                            value={value}
+                            maxLength={50}
+                        />
+                    )}
+                    name="email"
                 />
+                {errors.email && <Text style={stylesForms.errorMessage}>{errors.email.message}</Text>}
 
-                <TextInput
-                    style={stylesForms.input}
-                    placeholder='Password'
-                    secureTextEntry={true}
+                <Controller
+                    control={control}
+                    rules={{
+                        maxLength: 50,
+                        required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                            style={stylesForms.input}
+                            placeholder='Password'
+                            secureTextEntry={true}
+                            onBlur={onBlur}
+                            onChangeText={onChange}
+                            value={value}
+                            maxLength={50}
+                        />
+                    )}
+                    name="password"
                 />
+                {errors.password && <Text style={stylesForms.errorMessage}>Password is required</Text>}
 
-                <View style={stylesForms.buttonWrap}><Button
-                    title="Login"
-                /></View>
+                <View style={stylesForms.buttonWrap}>
+                    <Button title="Login" onPress={handleSubmit(submitHandler)}/>
+                </View>
             </>
         </ContentPageFrame>
     );
@@ -44,6 +93,9 @@ const stylesForms = StyleSheet.create({
         marginTop: 12,
         marginBottom: 12,
         width: 70
+    },
+    errorMessage: {
+        color: 'red'
     }
 });
 
