a05edc8022adba0ebdc1d891978217d6b108a98f
krasnoff <krasnoff.kobi@gmail.com>
2024-12-17 17:34:24 +0200

    Update get-read-list-store.ts

--- store/actions/get-read-list-store.ts
+++ store/actions/get-read-list-store.ts
@@ -1,1 +1,5 @@
+import { GET_READ_LIST_STORED, READ_LIST_STORED_LOADED } from "../action-type";
 
+export function getReadListStore() {
+    return { type: GET_READ_LIST_STORED, url: `/manga/list`, target: READ_LIST_STORED_LOADED };
+}
