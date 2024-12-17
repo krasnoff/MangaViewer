b84d05e09bbcfe2aa442ecc95e1ca0fe6cf20a15
krasnoff <krasnoff.kobi@gmail.com>
2024-12-17 17:42:58 +0200

    Update get-read-list-store.ts

--- store/reducers/get-read-list-store.ts
+++ store/reducers/get-read-list-store.ts
@@ -1,1 +1,14 @@
+import { GET_READ_LIST_STORED } from "../action-type";
+import initialState from "../state";
 
+const GetReadListStoreReducer = (state = initialState, action: any) => {
+    if (action.type == GET_READ_LIST_STORED) {
+        return Object.assign({}, state, {
+            readListStoredResponse: state.readListStoredResponse = action.payload
+        });
+    }
+
+    return state;
+}
+
+export default GetReadListStoreReducer;
