7d8c1742235641aaf2e5754aeb437dc5eaaad9af
krasnoff <krasnoff.kobi@gmail.com>
2025-01-07 18:44:14 +0200

    add naviagation to item page

--- hooks/useInitialUrl.ts
+++ hooks/useInitialUrl.ts
@@ -1,9 +1,26 @@
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
 import { useEffect, useState } from "react";
 import { Linking } from "react-native";
 
+type ItemScreenNavigationProp = StackNavigationProp<any, 'Item'>;
+
 export function useInitialUrl() {
+    const navigation = useNavigation<ItemScreenNavigationProp>();
+
+    const extractGuid = (url: string) => {
+        const match = url.match(/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/i);
+        return match ? match[0] : '';
+    }
+
     const handleDeepLink = (initialUrl: string) => {
         console.log('handleDeepLink', initialUrl);
+
+        // TODO - sample deep link URL: mangaViewer://item/b227745e-ffbc-4e23-88d0-5f92a538fea1
+        
+        navigation.navigate('Item', {
+            itemId: extractGuid(initialUrl),
+        });
     }
 
     // Handle both initial URL and subsequent deep links
